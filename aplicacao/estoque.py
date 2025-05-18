8a6ad2be18ad9f50562946aa1eb4bedd0a079665
Thiago de Paula Souza <75866888+ThiagodePaulaSouza@users.noreply.github.com>
2025-05-18 21:26:38 +0000

    separado logica na aplicacao

---
+++ aplicacao/estoque.py
@@ -0,0 +1,9 @@
+class Estoque:
+    def __init__(self):
+        pass
+
+    def adicionar_estoque(self):
+        print('adicionar_estoque')
+
+    def remover_estoque(self):
+        print('remover_estoque')
